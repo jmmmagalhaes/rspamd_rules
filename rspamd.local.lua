d384b4eb2c3bbbd7523633cf76d173cfa020ca04
Jorge Magalhães <jmagalhaes@wdata.pt>
2025-10-05 14:22:47 +0100

    Update rspamd.local.lua

--- rspamd.local.lua
+++ rspamd.local.lua
@@ -6,7 +6,7 @@ local rspamd_re = require "rspamd_regexp"
 local rspamd_hash = require "rspamd_cryptobox_hash"
 local rspamd_util = require "rspamd_util"
 
-local check_cw_dns = '._cw.k445lw5de3fxoxajjdmpftdqdy.hbl.dq.spamhaus.net.'
+local check_cw_dns = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 local function check_cw_callback ( task, re, lowercase, cryptovalue )
     local parts = task:get_text_parts()
